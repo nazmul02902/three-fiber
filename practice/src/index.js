77bd949085008dd263473c5a67cbbb32c0ad36da
nazmul02902 <nazmulhasan02902@gmail.com>
2023-03-04 16:41:18 +0000

    main

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, SpotLight } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, SpotLight } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -8,12 +8,10 @@ root.render(
   <Canvas style={{ height: "100vh" }}>
     <OrbitControls />
     {/* <ambientLight intensity={0.9}/> */}
-    <directionalLight color="red" position={[1, 1, 1]} />
-    <mesh>
-      <boxGeometry />
-      <meshStandardMaterial />
+    {/* <directionalLight color="red" position={[1, 1, 1]} /> */}
+    <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+    <mesh rotation-x={-Math.PI * 0.5}>
+      <planeGeometry args={[3, 3]} />
     </mesh>
-    <SpotLight position={[3, 3, 0]} castShadow  />
-    <SpotLight position={[-3, 3, 0]} color={[1, 0.25, 0.7]} angle={0.6} castShadow  />
   </Canvas>
 );
