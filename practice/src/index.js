47e5f1f9cada995920d17150772b9d984f3dd8a3
nazmul02902 <nazmulhasan02902@gmail.com>
2023-03-07 15:25:48 +0000

    main

--- practice/src/index.js
+++ practice/src/index.js
@@ -6,12 +6,30 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Canvas style={{ height: "100vh" }}>
-    <OrbitControls />
-    {/* <ambientLight intensity={0.9}/> */}
-    {/* <directionalLight color="red" position={[1, 1, 1]} /> */}
+    <color attach={"background"} args={[0, 0, 0]} />
+    <OrbitControls maxPolarAngle={1.45} />
+    {/* <spotLight
+     color={[1, 0.25, 0.7]}
+     intensity={1.5}
+     angle={0.6}
+     penumbra={0.5}
+     position={[5, 5, 0]}
+     castShadow
+     shadow-bias={-0.0001}
+    />
+    <spotLight
+     color={[1, 0.25, 0.7]}
+     intensity={1.5}
+     angle={0.6}
+     penumbra={0.5}
+     position={[-5, 5, 0]}
+     castShadow
+     shadow-bias={-0.0001}
+    /> */}
     <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
     <mesh rotation-x={-Math.PI * 0.5}>
-      <planeGeometry args={[3, 3]} />
+      <planeGeometry args={[10, 10]} />
+      {/* <meshPhongMaterial attach="material" color="red" /> */}
     </mesh>
   </Canvas>
 );
--- webpack/src/App.js
+++ webpack/src/App.js
@@ -2,15 +2,17 @@ import { Canvas } from "@react-three/fiber";
 import {  OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
 import Ground from "./components/Ground";
+import { Car } from "./components/Car";
 
 const CarShow = () => {
   return (
     <>
+    {/* <ambientLight/> */}
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
       <color attach={"background"} args={[0, 0, 0]} />
       <Ground/>
-    
+      <Car/>
       <spotLight
         color={[1, 0.25, 0.7]}
         intensity={1.5}
